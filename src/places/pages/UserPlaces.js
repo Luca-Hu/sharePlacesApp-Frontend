f3d52f008ecc61a6de3939825cfc282528b5f5ba
Yi Hu <Huuee9742@Gmail.com>
2022-08-22 23:20:57 -0500

    Update UserPlaces.js
    Fixed an issue where a sign-in could not see Places for other users. Error reason: Because userId was incorrectly provided by Auth.context instead of url.params.

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,29 +1,32 @@
-import React, { useEffect , useState ,useContext} from 'react';
+import React, { useEffect , useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { useHttpClient } from '../../shared/hooks/http-hook';
-
-import { AuthContext } from '../../shared/context/auth-context';
+import './PlaceForm.css';
 
 const UserPlaces = () => {
   const [loadedPlaces, setLoadedPlaces] = useState();
 
   const {isLoading, error, sendRequest, clearError} = useHttpClient();
 
-  const auth = useContext(AuthContext); // 从AuthContext 中拿取登录者 id
+  // const auth = useContext(AuthContext); // 从AuthContext 中拿取登录者 id 
+  //  debug: userplaces 不止是拜访登录者自己的places，而可以是浏览其他人的places，此时只能从url中“userParams”取userId，而不能从“authContext”（仅有登录者自己）中取
+
+  const userId = useParams().userId;
 
   useEffect(() => {
     const fetchPlaces = async() => {
       try {
-        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${auth.userId}`);
+        const responseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`);
         setLoadedPlaces(responseData.places); // 从Backend 得到的数据为： 一个带places key 的对象
         console.log(responseData.places);
       } catch (error) {}
     }; // 使用fetchPlaces把 sendRequest 包装为一个异步函数，因为useEffect 不是
     fetchPlaces();
-  }, [sendRequest, auth.userId]); // useEffect： dependency只会被创建一次，此后不变。仅在第一次render的时候，useEffect中的sendRequest才被调用一次，后续的re-render时，sendRequest将不再被调用。也即：req最多只发送一次。
+  }, [sendRequest, userId]); // useEffect： dependency只会被创建一次，此后不变。仅在第一次render的时候，useEffect中的sendRequest才被调用一次，后续的re-render时，sendRequest将不再被调用。也即：req最多只发送一次。
   // 把userId 也作为 dependency， 因为网页的URL 不会改变
   
   const placeDeletedHandler = deletedPlaceId => { // 返回过滤掉被删除的place 的places
